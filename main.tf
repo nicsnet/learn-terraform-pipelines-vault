e2c06df77bd46801411a298ea1bd36332aadc090
Nicola Sheldrick <sheldr@gmail.com>
2020-10-12 23:49:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"
+    organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "robin-n-vault"
+      name = "nicola-s-vault"
     }
   }
 }
